17d8571017850af36438011a357a250dc2e0b393
dzuvan <hrnjak.jovan1@gmail.com>
2018-01-25 19:40:53 +0100

    Imporved response text.

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,24 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then((docs) => {
-            console.log(docs);
-            res.status(200).json(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map((doc) => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                }),
+            };
+            res.status(200).json(response);
         })
         .catch((err) => {
             console.log(err);
@@ -30,8 +44,16 @@ router.post('/', (req, res, next) => {
         .then((result) => {
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST request to /products',
-                createdProduct: result,
+                message: 'Created product',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    reqeust: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                },
             });
         }).catch((err) => {
             console.log(err);
@@ -42,18 +64,26 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then((doc) => {
             console.log("From db: ", doc);
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        description: 'Get all products',
+                        url: 'http://localhost:3000/products/',
+                    }
+                });
             } else {
                 res.status(404).json({ message: "No valid product found" });
             }
         })
         .catch((err) => {
             console.log(err);
-            res.status.apply(500).json({
+            res.status(500).json({
                 error: err,
             })
         });
@@ -68,8 +98,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then((result) => {
-            console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
         })
         .catch((err) => {
             console.log(err);
@@ -84,7 +119,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
         .exec()
         .then((result) => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products',
+                    data: { name: 'String', price: 'Number' }
+                }
+            });
         })
         .catch((err) => {
             console.log(err);
@@ -93,6 +135,5 @@ router.delete('/:productId', (req, res, next) => {
             });
         });
 });
-
 
 module.exports = router;
--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb+srv://sys:' + process.env.MONGO_ATLAS_PW + '@node-isa-i1kmg.mongodb.net/test');
-mongoose.connect('mongodb://sys:'+ process.env.MONGO_ATLAS_PW + '@node-isa-shard-00-00-i1kmg.mongodb.net:27017,node-isa-shard-00-01-i1kmg.mongodb.net:27017,node-isa-shard-00-02-i1kmg.mongodb.net:27017/test?ssl=true&replicaSet=node-isa-shard-0&authSource=admin')
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
+
+// mongoose.connect('mongodb+srv://sys:' + process.env.MONGO_ATLAS_PW + '@node-isa-i1kmg.mongodb.net/test');
+mongoose.connect('mongodb://sys:'+ process.env.MONGO_ATLAS_PW + '@node-isa-shard-00-00-i1kmg.mongodb.net:27017,node-isa-shard-00-01-i1kmg.mongodb.net:27017,node-isa-shard-00-02-i1kmg.mongodb.net:27017/test?ssl=true&replicaSet=node-isa-shard-0&authSource=admin')
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
